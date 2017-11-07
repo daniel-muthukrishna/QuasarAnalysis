0d9d4d997e1f01704b3542655520b12ea0377b11
danmuth <daniel.muthukrishna@gmail.com>
2017-11-07 01:32:23 +0000

    K-means clustering algorithm implemented and plotted onto a grid plot

--- do_statistics.py
+++ do_statistics.py
@@ -20,7 +20,7 @@ def compare_reconstruction_and_data(name, spectra):
     sdssFlux = smooth(spectra[name]['sdssFlux'])
     reconFlux = spectra[name]['reconFlux']
     pearsonCorr = round(pearsonr(sdssFlux, reconFlux)[0], 3)
-    chi2 = round(chisquare(sdssFlux, reconFlux)[0], 0)
+    chi2 = 0  # round(chisquare(sdssFlux, reconFlux)[0], 0)
 
     return pearsonCorr, chi2
 
@@ -73,12 +73,12 @@ def plot_weights(spectra, removeNames):
     weightsDF = get_weights(spectra, removeNames, lineType='All')
     weightsDFBals = get_weights(spectra, removeNames, lineType='BALs')
     weightsDFNonBals = get_weights(spectra, removeNames, lineType='nonBALs')
-    # corner.corner(weightsDF)
+    corner.corner(weightsDF)
 
     c = ChainConsumer()
     c.add_chain(weightsDFBals.values, parameters=weightsDF.columns.tolist())
     c.add_chain(weightsDFNonBals.values, parameters=weightsDF.columns.tolist())
-    c.add_chain(weightsDF.values, parameters=weightsDF.columns.tolist())
+    # c.add_chain(weightsDF.values, parameters=weightsDF.columns.tolist())
 
     c.plotter.plot()
 
@@ -89,11 +89,46 @@ def plot_weights(spectra, removeNames):
     #             plt.scatter(weightsDF[label1], weightsDF[label2])
 
 
-def clustering(spectra, removeNames):
-    kmeans = KMeans(n_clusters=2)
+def clustering(spectra, removeNames, numClusters=3):
     weightsDF = get_weights(spectra, removeNames)
+    weightNames = weightsDF.columns.tolist()
 
+    kmeans = KMeans(n_clusters=numClusters)
     kmeans.fit(weightsDF)
+    labels = kmeans.predict(weightsDF)
+    centroids = kmeans.cluster_centers_
+    centroidsDF = pd.DataFrame(data=centroids, columns=weightNames)
+
+    fig, ax = plt.subplots(nrows=6, ncols=6, sharex='col', sharey='row')
+    fig.subplots_adjust(wspace=0, hspace=0)
+    colmap = {1: 'r', 2: 'g', 3: 'b', 4: 'c'}
+    colors = list(map(lambda x: colmap[x + 1], labels))
+
+    for i, label1 in enumerate(weightNames):
+        for j, label2 in enumerate(weightNames):
+            if label1 != label2 and i >= j:
+                ax[i, j].scatter(weightsDF[label1], weightsDF[label2], color=colors, alpha=0.3)
+                for idx, centroid in centroidsDF.iterrows():
+                    pass  #ax[i, j].scatter(centroid[label1], centroid[label2], color=colmap[idx+1], s=20)
+                if i == 5:
+                    ax[i, j].set_xlabel(label2)
+                if j == 0:
+                    ax[i, j].set_ylabel(label1)
+
+    clusters = {}
+    for i in range(numClusters):
+        clusters[labels[i]] = []
+    for idx, row in weightsDF.iterrows():
+        clusters[labels[idx]].append(row.values)
+    for i in range(numClusters):
+        clusters[labels[i]] = np.array(clusters[labels[i]])
+
+    c = ChainConsumer()
+    for clusterName in clusters.keys():
+        c.add_chain(clusters[clusterName], parameters=weightNames)
+    c.plotter.plot()
+
+
 
 
 if __name__ == '__main__':
